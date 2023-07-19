12469dc96f46a6b1c79d20b2c67f9157bfe87419
stevan-li <fengtao.li@tigermedgrp.com>
2023-07-19 17:21:20 +0800

    fix: commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,49 @@
-import { defineConfig } from 'vite';
+// vite.config.js
+import { defineConfig, loadEnv } from 'vite'; // 使用 defineConfig 工具函数获取类型提示：
 import vue from '@vitejs/plugin-vue';
-export default defineConfig({
-  plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // 这里可以添加全局的 Sass 变量、Mixin等。首先你的有这个文件// additionalData: `//   @import "@/styles/variables.scss";//   @import "@/styles/mixins.scss";// `,
+import path from 'path';
+// import { viteMockServe } from 'vite-plugin-mock';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: './', // 开发或生产环境服务的公共基础路径
+    optimizeDeps: {
+      force: true, // 强制进行依赖预构建
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import '/src/assets/styles/variables.scss';`, // 引入全局变量文件
+        },
       },
     },
-  },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'), // 路径别名
+      },
+      extensions: ['.js', '.ts', '.json'], // 导入时想要省略的扩展名列表
+    },
+    server: {
+      host: true, // 监听所有地址
+      port: 8000,
+      open: true,
+      proxy: {
+        [env.VUE_APP_BASE_API]: {
+          target: `http://localhost:8080`,
+          // target: `http://10.0.55.93:8100/prod-api`,
+          changeOrigin: true,
+          pathRewrite: {
+            ['^' + env.VUE_APP_BASE_API]: '',
+          },
+        },
+      },
+    },
+    build: {
+      outDir: 'build', // 打包文件的输出目录
+      assetsDir: 'static', // 静态资源的存放目录
+      assetsInlineLimit: 4096, // 图片转 base64 编码的阈值
+    },
+    plugins: [vue()],
+  };
 });
